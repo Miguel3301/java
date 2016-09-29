d3a60dbd7f7b593b75196aef60e82238c69e9b00
Miguel3301 <diazm4903@gmail.com>
2016-09-29 10:23:44 -0700

    Add files via upload

---
+++ Asc.java
@@ -0,0 +1,18 @@
+// file; Asc.java
+// javac Asc.java
+// java Asc
+class Asc {
+    public static void main (String a[]){
+		//variables
+		int i;// integer i
+		char c;// character c
+		//for loop
+		for (i = 0; i < 256; i++){
+			c = (char)i;
+			System.out.println(i);
+			System.out.print(" ");
+			System.out.print(c);
+				System.out.print(" ");
+		}
+    }
+}
